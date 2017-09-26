722131eed17a0df4d1f953f6442bd6b5e726a91b
Stephan Geulette <s.geulette@imio.be>
2017-09-26 10:08:29 +0000

    Added code to do corrections

--- Extensions/corrections.py
+++ Extensions/corrections.py
@@ -36,3 +36,21 @@ def correct_registry(self, dochange=''):
 """
 {'_value': [{'fct_title': u"Gestionnaire d'action", 'fct_id': u'actioneditor'}], '__name__': 'collective.contact.plonegroup.browser.settings.IContactPlonegroupConfig.functions', '_field': <plone.registry.field.List object at 0x7fc88c20a320>, '__parent__': <Registry at portal_registry>, '__provides__': <zope.interface.Provides object at 0x7fc88c208350>}
 """
+
+
+def various(self):
+    """
+        corrections diverses avec ipdb
+    """
+    # Changer le default d'un tb, ici ne rien mettre au niveau le plus haut
+    context = self
+    from imio.dashboard.utils import getCollectionLinkCriterion
+    criterion = getCollectionLinkCriterion(context)
+    criterion.default = u''
+    from eea.facetednavigation.criteria.interfaces import ICriteria
+    ICriteria(context).criteria._p_changed = True
+
+    # Réparer la vue de la page pst
+    context.setLayout('view')
+    from imio.project.pst.setuphandlers import configure_faceted_folder
+    configure_faceted_folder(context, xml='default_dashboard_widgets.xml', default_UID=None)
